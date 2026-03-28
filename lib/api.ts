210bbc93b4e4af60c1c30c36bc2ba0c2e75d90fd
Kazuya Ogawa <jiashouyexiaochuan@gmail.com>
2026-03-28 12:36:19 +0900

    API

--- lib/api.ts
+++ lib/api.ts
@@ -9,5 +9,6 @@ export const apiFetch = async (path: string, options: RequestInit = {}) => {
             ...options.headers,
         },
     });
+    console.log("API_BASE_URL:", API_BASE_URL);
     return res;
 };
