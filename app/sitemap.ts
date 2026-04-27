a52eb133566934d1b1a4c9312a5ed8323d65cdda
Kazuya Ogawa <jiashouyexiaochuan@gmail.com>
2026-04-27 21:04:53 +0900

    サイトマップ修正

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,13 +4,5 @@ export default function sitemap() {
             url: 'https://programing-quiz-zeta.vercel.app/',
             lastModified: new Date(),
         },
-        {
-            url: 'https://programing-quiz-zeta.vercel.app/questions',
-            lastModified: new Date(),
-        },
-        {
-            url: 'https://programing-quiz-zeta.vercel.app/questions/{id}',
-            lastModified: new Date(),
-        },
     ];
 }
