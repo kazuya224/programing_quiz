2df7c15944d6dde996404cef27733818e0bb2125
Kazuya Ogawa <jiashouyexiaochuan@gmail.com>
2026-05-15 07:01:57 +0900

    sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,8 +1,39 @@
-export default function sitemap() {
+import { MetadataRoute } from "next";
+
+type Problem = {
+    questionId: string;
+};
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+    const res = await fetch(
+        "https://api.devtrain-app.com/api/sitemap/problems",
+        // "http://localhost:8080/api/sitemap/problems",
+        {
+            next: {
+                revalidate: 86400,
+            },
+        }
+    );
+
+    if (!res.ok) {
+        throw new Error(
+            `Failed to fetch problems: ${res.status}`
+        );
+    }
+
+    const problems: Problem[] = await res.json();
+
+    console.log("problems count:", problems.length);
+
     return [
         {
-            url: 'https://programing-quiz-zeta.vercel.app/',
+            url: "https://app.devtrain-app.com",
             lastModified: new Date(),
         },
+
+        ...problems.map((problem) => ({
+            url: `https://app.devtrain-app.com/questions/${problem.questionId}`,
+            lastModified: new Date(),
+        })),
     ];
 }
